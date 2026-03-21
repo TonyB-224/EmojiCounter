ac46bbd3c4e3786c5d48561d01fb4e5cb836832b
TonyB-224 <tbullard224@gmail.com>
2026-03-21 12:59:47 -0500

    Initial Commit

---
+++ EmojiCounter/EmojiCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiCounterApp.swift
+//  EmojiCounter
+//
+//  Created by user942908 on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct EmojiCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
